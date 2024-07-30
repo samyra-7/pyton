8d477c6d5ffadd3d54f024e6da5b8664a9ee3168
Samyra <dasilvasamyra3@gmail.com>
2024-07-30 14:51:33 -0300

    Add files via upload

---
+++ app.py
@@ -0,0 +1,32 @@
+print('Ola mundo!')
+
+numero_inteiro= 42
+print(numero_inteiro)
+
+numero_float= 2.5
+print(numero_float)
+
+string= "essa é uma variavel do tipo de dados string!"
+
+condicao= True
+
+nome= input("escreva o seu nome:")
+print(nome)
+
+idade= int(input("escrava a sua idade:"))
+print(idade)
+
+num1= float(input("escreva um numero"))
+num2= float(input("escreva outro numero"))
+
+soma= num1 + num2
+subtracao= num1 - num2
+multiplicacao= num1 * num2
+divisao= num1 / num2
+
+print(f"soma: {soma}")
+print(f"Subtração: {subtracao}")
+print(f"Multiplicação: {multiplicacao}")
+print(f"Divisão: {divisa}")
+
+
---
+++ aula01.py
@@ -0,0 +1,13 @@
+#
+
+n1= float (input("Escreva um número: "))
+n2= float (input("Escreva outro número: "))
+
+soma= n1 + n2
+sub= n1 - n2 
+
+print(f"O resultado da soma é: " {soma}")
+prin
+ 
+
+
